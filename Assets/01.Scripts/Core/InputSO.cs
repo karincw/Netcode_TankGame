26d4a056a1341a49bcb3a6fd590ff91d75dc4fc5
Karin <karin.cw07@gmail.com>
2024-03-20 08:30:41 +0900

    유니티다운로드

---
+++ Assets/01.Scripts/Core/InputSO.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+using static PlayerInput;
+
+[CreateAssetMenu(fileName = "Karin/InputSO")]
+public class InputSO : ScriptableObject, IPlayerActions, IUIActions
+{
+    #region Event Section
+
+    public event Action<Vector2> OnMovementEvent;
+    public event Action<bool> OnFireEvent;
+
+    #endregion
+    #region Velue Section
+
+    #endregion
+
+    private PlayerInput _inputSystem;
+    public PlayerInput InputSystem => _inputSystem;
+
+    private void OnEnable()
+    {
+        if (_inputSystem == null)
+        {
+            _inputSystem = new PlayerInput();
+            _inputSystem.Player.SetCallbacks(this);
+            _inputSystem.UI.SetCallbacks(this);
+        }
+
+        _inputSystem.Player.Enable();
+        //_inputSystem.UI.Enable();
+    }
+
+    #region PlayerAction
+
+    public void OnMovement(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Vector2 movement = context.ReadValue<Vector2>();
+            OnMovementEvent?.Invoke(movement);
+        }
+    }
+
+    public void OnFire(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            OnFireEvent?.Invoke(true);
+        }
+        else if (context.canceled)
+        {
+            OnFireEvent?.Invoke(false);
+        }
+    }
+
+    #endregion
+}
